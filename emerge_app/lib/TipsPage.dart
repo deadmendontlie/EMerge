c3bfc1b6de6b45089b0948f6116016f1d0586cef
FBRob <55197274+FBRob@users.noreply.github.com>
2020-03-05 12:25:19 -0500

    Add files via upload

--- emerge_app/lib/TipsPage.dart
+++ emerge_app/lib/TipsPage.dart
@@ -15,7 +15,7 @@ class _TipsPageWidgetState extends State<TipsPage> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Page 4'),
+          title: Text('Anonymous Tips'),
           leading: IconButton(
             icon: Icon(Icons.arrow_back_ios),
             tooltip: 'Back',
@@ -24,6 +24,41 @@ class _TipsPageWidgetState extends State<TipsPage> {
             },
           ),
         ),
+        body: Column(
+          children: [
+            Text(
+              "Please Select What Services are Required",
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "Please select the type of activity.",
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "Please enter a description of the activity.",
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "Please enter your name otherwise this will be submitted Anonymously",
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "Please submit your phone number(Not required)",
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+
+            Center(
+              child: RaisedButton(
+                child: Text('Submit'),
+                onPressed: () {
+                  // Add Submission results later
+
+                },
+              ),
+            ),
+          ],
+        ),
+
       ),
     );
   }
