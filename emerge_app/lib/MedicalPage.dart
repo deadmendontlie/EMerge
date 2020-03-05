e3d6739f2507a948a81682d4dc9008dde198b408
Deadmendontlie <33693665+deadmendontlie@users.noreply.github.com>
2020-03-04 19:15:33 -0500

    Add files via upload

--- emerge_app/lib/MedicalPage.dart
+++ emerge_app/lib/MedicalPage.dart
@@ -17,7 +17,7 @@ class _MedicalPageWidgetState extends State<MedicalPage> {
       home: Scaffold(
         //blue header
         appBar: AppBar(
-          title: Text('Medical Report Screen'),
+          title: Text('Medical Report'),
           leading: IconButton(
             icon: Icon(Icons.arrow_back_ios),
             tooltip: 'Back',
@@ -26,45 +26,60 @@ class _MedicalPageWidgetState extends State<MedicalPage> {
             },
           ),
         ),
-        body: Column(
-            children: [
-              //Remember later to center these and to add the scrolling functionality to the screen and put column inside it
-              //Also add the drop downs and stuff later
-              //Once this is all done and we have the http methods and stuff add them later and maybe have it make a json file on submit
-                Text(
-                    "Please Select What Services are Required(Defaults to Just Medical)",
-                    style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                    "Please Select What Services are Required",
-                    style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                    "Please Select Type of Report",
-                    style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                    "Please enter your name otherwise this will be submitted Anonymously",
-                    style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                    "Please submit your phone number(Not required)",
-                    style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                    "Enter any additional information below",
-                     style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-                ),
-                Center(
-                    child: RaisedButton(
-                        child: Text('Submit'),
-                        onPressed: () {
-                        // Add Submission results later
+        body: SingleChildScrollView(
+          child: Column(
+              children: [
+                //TODO Remember later to center these and to add the scrolling functionality to the screen and put column inside it
+                //TODO Also add the drop downs and stuff later
+                //TODO Once this is all done and we have the http methods and stuff add them later and maybe have it make a json file on submit
+                //TODO Add the drop down menus and fill them with info
+                //TODO Make the text boxes nice and put extra info in them if needed
+                //TODO Add other things need for the text boxes and drop downs
+                //TODO Make the submit button work
+                  Text(
+                      "Please Select What Services are Required(Defaults to Just Medical)",
+                      style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.normal),
+                  ),
+                  Text(
+                      "Please Select What Services are Required",
+                      style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.normal),
+                  ),
+                  Text(
+                      "Please Select Type of Report",
+                      style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.normal),
+                  ),
+                  Text(
+                      "Please enter your name otherwise this will be submitted Anonymously",
+                      style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.normal),
+                  ),
+                  TextFormField(textAlign: TextAlign.left, autocorrect: true, showCursor: true
 
-                        },
-                    ),
-                ),
-            ],
+                  ),
+                  Text(
+                      "Please submit your phone number(Not required)",
+                      style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.normal),
+                  ),
+                  TextFormField(textAlign: TextAlign.left, autocorrect: true, showCursor: true
+
+                  ),
+                  Text(
+                      "Enter any additional information below",
+                       style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.normal),
+                  ),
+                  TextFormField(textAlign: TextAlign.left, autocorrect: true, showCursor: true
+
+                  ),
+                  Center(
+                      child: RaisedButton(
+                          child: Text('Submit'),
+                          onPressed: () {
+                          // Add Submission results later
+
+                          },
+                      ),
+                  ),
+              ],
+          ),
         ),
       )
     );
--- emerge_app/lib/PolicePage.dart
+++ emerge_app/lib/PolicePage.dart
@@ -15,7 +15,7 @@ class _PolicePageWidgetState extends State<PolicePage> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Page 2'),
+          title: Text('Police Report'),
           leading: IconButton(
             icon: Icon(Icons.arrow_back_ios),
             tooltip: 'Back',
@@ -24,42 +24,57 @@ class _PolicePageWidgetState extends State<PolicePage> {
             },
           ),
         ),
-        body: Column(
-            children: [
-                Text(
-                    "Please Select What Services are Required(Defaults to Just Medical)",
-                    style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                    "Please Select What Services are Required",
-                    style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                    "Please Select Type of Report",
-                    style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                    "Please enter your name otherwise this will be submitted Anonymously",
-                    style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                    "Please submit your phone number(Not required)",
-                    style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                    "Enter any additional information below",
-                     style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-                ),
-                Center(
-                    child: RaisedButton(
-                        child: Text('Submit'),
-                        onPressed: () {
-                        // Add Submission results later
+        body: SingleChildScrollView(
+          child: Column(
+              children: [
+                //TODO Add the drop down menus and fill them with info
+                //TODO Make the text boxes nice and put extra info in them if needed
+                //TODO Add other things need for the text boxes and drop downs
+                //TODO Make the submit button work
+                  Text(
+                      "Please Select What Services are Required(Defaults to Just Medical)",
+                      style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.normal),
+                  ),
+                  Text(
+                      "Please Select What Services are Required",
+                      style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.normal),
+                  ),
+                  Text(
+                      "Please Select Type of Report",
+                      style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.normal),
+                  ),
+                  Text(
+                      "Please enter your name otherwise this will be submitted Anonymously",
+                      style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.normal),
+                  ),
+                  TextFormField(textAlign: TextAlign.left, autocorrect: true, showCursor: true
 
-                        },
-                    ),
-                ),
-            ],
+                  ),
+                  Text(
+                      "Please submit your phone number(Not required)",
+                      style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.normal),
+                  ),
+                  TextFormField(textAlign: TextAlign.left, autocorrect: true, showCursor: true
+
+                  ),
+                  Text(
+                      "Enter any additional information below",
+                       style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.normal),
+                  ),
+                  TextFormField(textAlign: TextAlign.left, autocorrect: true, showCursor: true
+
+                  ),
+                  Center(
+                      child: RaisedButton(
+                          child: Text('Submit'),
+                          onPressed: () {
+                          // Add Submission results later
+
+                          },
+                      ),
+                  ),
+              ],
+          ),
         ),
       ),
     );
