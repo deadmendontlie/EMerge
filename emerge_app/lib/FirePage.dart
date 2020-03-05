303f96cfddbd945b1bf4c5ed1ffc0ff6b113322f
FBRob <55197274+FBRob@users.noreply.github.com>
2020-03-05 12:25:48 -0500

    Add files via upload

--- emerge_app/lib/FirePage.dart
+++ emerge_app/lib/FirePage.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/material.dart';
 class FirePage extends StatefulWidget{
   @override
@@ -15,7 +17,7 @@ class _FirePageWidgetState extends State<FirePage> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Page 3'),
+          title: Text('Fire Page'),
           leading: IconButton(
             icon: Icon(Icons.arrow_back_ios),
             tooltip: 'Back',
@@ -24,6 +26,44 @@ class _FirePageWidgetState extends State<FirePage> {
             },
           ),
         ),
+        body: Column(
+          children: [
+            Text(
+              "Please Select What Services are Required(Defaults to just Fire Report)",
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "Please select what additional services are required",
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "Please Select Type of Report",
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "Please enter your name otherwise this will be submitted Anonymously",
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "Please submit your phone number(Not required)",
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "Enter any additional information below",
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+            Center(
+              child: RaisedButton(
+                child: Text('Submit'),
+                onPressed: () {
+                  // Add Submission results later
+
+                },
+              ),
+            ),
+          ],
+        ),
+
       ),
     );
   }
