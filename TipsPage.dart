d4a8d3d7cc362c1a0b530aa15ae4a1a56f287075
Deadmendontlie <33693665+deadmendontlie@users.noreply.github.com>
2020-03-17 11:15:42 -0400

    Add files via upload

---
+++ TipsPage.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class TipsPage extends StatefulWidget {
+  @override
+  _TipsPageWidgetState createState() => _TipsPageWidgetState();
+}
+
+class _TipsPageWidgetState extends State<TipsPage> {
+  @override
+  initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Tips Page'),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios),
+            tooltip: 'Back',
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              //TODO Add the drop down menus and fill them with info
+              //TODO Make the text boxes nice and put extra info in them if needed
+              //TODO Add other things need for the text boxes and drop downs
+              //TODO Make the submit button work
+              Text(
+                "Please enter your name otherwise this will be submitted Anonymously",
+                style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.normal),
+              ),
+              TextFormField(
+                textAlign: TextAlign.left,
+                autocorrect: false,
+                showCursor: true,
+                decoration: new InputDecoration.collapsed(
+                    hintText: "Please enter your name"),
+              ),
+              Text(
+                "Please submit your phone number(Not required)",
+                style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.normal),
+              ),
+              TextFormField(
+                textAlign: TextAlign.left,
+                autocorrect: false,
+                showCursor: true,
+                decoration: new InputDecoration.collapsed(
+                    hintText: "Please enter phone number"),
+              ),
+              Text(
+                "Enter what you would like to report for the tip.",
+                style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.normal),
+              ),
+              TextFormField(
+                textAlign: TextAlign.left,
+                autocorrect: true,
+                showCursor: true,
+                decoration: new InputDecoration.collapsed(
+                    hintText: "Tip Information"),
+              ),
+              Center(
+                child: RaisedButton(
+                  child: Text('Submit'),
+                  onPressed: () {
+                    // Add Submission results later
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
