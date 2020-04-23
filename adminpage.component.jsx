d451d2e441963ed7f7e7b1fcfee08d0e62c6129e
PennNguyen <34245455+PennNguyen@users.noreply.github.com>
2020-04-23 02:04:33 -0400

    Add files via upload
    Updated admin pages with changes
    
        Tab 1 - Add Municipality
            Text fields - Name, State, Location, Email
            Submit button
        Tab 2 - Add Service
            Text fileds - Name, State, Location
            Dropdown - Service Type
            Submit button
        Tab 3 - Link Municipality
            Dropdowns - Municipality, Police, Fire, EMS
            Submit button
        Tab 4 - Municipalities and Services
            List or table containing all municipalities and their corresponding services

--- adminpage.component.jsx
+++ adminpage.component.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Tabs, useTabState, Panel } from "@bumaga/tabs";
+
 import './adminpage.styles.scss';
-//import FormInput from '../../components/form-input/form-input.component';
-
-
 
 const cn = (...args) => args.filter(Boolean).join(' ')
 
@@ -17,95 +15,169 @@ const Tab = ({ children }) => {
   )
 }
 
-
-
-const adminPage = ({ }) => (
- <div className = "adminpage">
+const AdminPage = () => (
+ <div className="adminpage">
      <Tabs>
-         <div class="tabs">
-             <div class="tab-list">
+         <div className="tabs">
+             <div className="tab-list">
                  <Tab>Add Municipality</Tab>
                  <Tab>Add Service</Tab>
+                 <Tab>Link Municipality</Tab>
                  <Tab>List of Municipality and Services</Tab>
              </div>
-             <div class="tab-progress"></div>
+             <div className="tab-progress"></div>
              <Panel> 
-                 <div classname ='municeForm'/>
+                 <div className ='municeForm'/>
                     <h1>Add needed districts</h1>
                     <form>
-                        <div class="title">Area:</div>
-                        <div class="infobox">
+                        <div className="title">Name:</div>
+                        <div className="infobox">
                           <input placeholder="" 
                               value="" 
-                              onChange={e => ({Area: e.target.value})}/>
+                              onChange={e => ({Name: e.target.value})}/>
                         </div>
 
-                        <div class="title">Medical:</div>
-                        <div class="infobox">
+                        <div className="title">State:</div>
+                        <div className="infobox">
                           <input placeholder="" 
                               value="" 
-                              onChange={e => ({Medical: e.target.value})}/>
+                              onChange={e => ({State: e.target.value})}/>
                         </div>
 
-                        <div class="title">Police:</div>
-                        <div class="infobox">
+                        <div className="title">Location:</div>
+                        <div className="infobox">
                           <input placeholder="" 
                               value="" 
-                              onChange={e => ({Police: e.target.value})}/>
+                              onChange={e => ({Location: e.target.value})}/>
                         </div>
 
-                        <div class="title">Fire Services:</div>
-                        <div class="infobox">
+                        <div className="title">Email:</div>
+                        <div className="infobox">
                           <input placeholder="" 
                               value="" 
-                              onChange={e => ({Fire: e.target.value})}/>
+                              onChange={e => ({Email: e.target.value})}/>
                         </div>
 
-                        <div class="button-container">
+                        <div className="button-container">
                         <button class="submit-button">Submit</button>
                 </div>
                     </form>
              </Panel>
              <Panel>
-             <h1>Add services for districts here</h1>
+             <div className ='serviceForm'/>
+              <h1>Add services for districts here</h1>
                     <form>
-                        <div class="title">Area:</div>
-                        <div class="infobox">
+                        <div className="title">Name:</div>
+                        <div className="infobox">
                           <input placeholder="" 
                               value="" 
-                              onChange={e => ({Area: e.target.value})}/>
+                              onChange={e => ({Name: e.target.value})}/>
                         </div>
 
-                        <div class="title">Medical:</div>
-                        <div class="infobox">
+                        <div className="title">State:</div>
+                        <div className="infobox">
                           <input placeholder="" 
                               value="" 
-                              onChange={e => ({Medical: e.target.value})}/>
+                              onChange={e => ({State: e.target.value})}/>
                         </div>
 
-                        <div class="title">Police:</div>
-                        <div class="infobox">
+                        <div className="title">Location:</div>
+                        <div className="infobox">
                           <input placeholder="" 
                               value="" 
-                              onChange={e => ({Police: e.target.value})}/>
+                              onChange={e => ({Location: e.target.value})}/>
                         </div>
 
-                        <div class="title">Fire Services:</div>
-                        <div class="infobox">
-                          <input placeholder="" 
-                              value="" 
-                              onChange={e => ({Fire: e.target.value})}/>
+                        <div className="title">Service Type:</div>
+                          <div className="dropbox">
+                          <select id="serviceType" name="Service Type"> 
+                          <option value="Fire">Fire</option>
+                          <option value="Medical">Medical</option>
+                          <option value="Police">Police</option>
+                          </select>
                         </div>
 
-                        <div class="button-container">
+                        <div className="button-container">
                         <button class="submit-button">Submit</button>
-                </div>
-                    </form>
+                    </div>
+                  </form>
              </Panel>
-             <Panel> INSERT LIST HERE</Panel>
+                      <Panel>
+                      <h1>Link the municipalities with specific services</h1>
+                        <div className="title">Municipality:</div>
+                          <div className="dropbox">
+                          <select id="municeType" name="Munice Type"> 
+                          <option value="Municipality 1">Municipality 1</option>
+                          <option value="Municipality 2">Municipality 2</option>
+                          <option value="Municipality 3">Municipality 3</option>
+                          </select>
+                        </div>
+
+                        <div className="title">Police Services:</div>
+                          <div className="dropbox">
+                          <select id="policeService" name="Police Service"> 
+                          <option value="police1">Police 1</option>
+                          <option value="police2">Police 2</option>
+                          <option value="police3">Police 3</option>
+                          </select>
+                        </div>
+
+                        <div className="title">Fire Services:</div>
+                          <div className="dropbox">
+                          <select id="fireService" name="Fire Service"> 
+                          <option value="Fire1">Fire 1</option>
+                          <option value="Fire2">Fire 2</option>
+                          <option value="Fire3">Fire 3</option>
+                          </select>
+
+                        <div className="title">EMS Services:</div>
+                          <div className="dropbox">
+                          <select id="emsService" name="EMS Service"> 
+                          <option value="EMS1">EMS 1</option>
+                          <option value="EMS2">EMS 2</option>
+                          <option value="EMS3">EMS 3</option>
+                          </select>
+                        </div>
+
+                        <div className="button-container">
+                        <button class="submit-button">Submit</button>
+                        </div>
+
+                        </div>
+                      </Panel>
+             <Panel>
+            <div className = "table">
+             <table>
+                <tr>
+                  <th>Municipality</th>
+                  <th>Fire Services</th>
+                  <th>EMS Services</th>
+                  <th>Police Services</th>
+                </tr>
+                <tr>
+                  <td>Glassboro District</td>
+                  <td>Glassboro Fire Department</td>
+                  <td>Glassboro Medical Center</td>
+                  <td>Glassboro Police Department</td>
+                </tr>
+                <tr>
+                  <td>Willingbro District</td>
+                  <td>Willingbro Fire Department</td>
+                  <td>Willingbro Medical Center</td>
+                  <td>Willingbro Poice Department</td>
+                </tr>
+                <tr>
+                  <td>Camden District</td>
+                  <td>Camden Fire Department</td>
+                  <td>Camden Medical Center</td>
+                  <td>Camden Poice Department</td>
+                </tr>
+            </table>
+            </div>
+             </Panel>
          </div>
      </Tabs>
  </div>
 )
 
-export default adminPage;
+export default AdminPage;
