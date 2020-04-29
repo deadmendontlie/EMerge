d74f3b1af7f9700514671adf11886fcfb6c22730
dumitr58 <55495556+dumitr58@users.noreply.github.com>
2020-04-29 12:04:10 -0400

    + 1 folder google maps and changed user table component
    created a new component called map and styles for it, this component parses the longitude and latitude coming in from the database into 2 separate variables, than makes use of the Google maps API and creates a specific map with a marker location corresponding to the longitude and latitude. Changed the user table component to create a popup window with the Google map location whenever Location is clicked.

---
+++ emerge/src/components/GMap/map.component.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import './map.styles.scss'
+
+const mapStyles = {
+  width: '100%',
+  height: '100%',
+};
+
+export class GMap extends React.Component {
+
+  render(){
+    // parsing the longitude and latitude from location field
+    // into 2 separate variables long and latitude
+    const latitude = this.props.location.substr(0,this.props.location.indexOf(',')).substr(5);
+    const long = this.props.location.substr(this.props.location.indexOf(',')).substr(8);
+  return (
+      <div className='mapStyles'>
+        {/* {console.log("location" + this.props.location)}
+        {console.log("lat" + latitude)}
+        {console.log("long"+ long)} */}
+        {/* Creating the Map and a Marker with the specified latitude and longitude */}
+        <Map className='mapStyles'
+          google={this.props.google}
+          zoom={8}
+          style={mapStyles}
+          initialCenter={{ lat: latitude, lng: long}}
+        >
+        <Marker position={{ lat: latitude, lng: long}} />
+        </Map>
+    </div>
+  );
+  }
+}
+// this is my google API key from GCP 
+// gives us access to the Map
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyCVYm77ZYuWWdbcQ2zrbEv_yg1DH_MyvJY'
+})(GMap);
